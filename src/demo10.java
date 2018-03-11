963e408584d2ef8b3e2c23dca30971a2e8a7091e
Alexandr Alexeev <fart82@ukr.net>
2018-03-11 21:02:11 +0200

    commit from IJ

---
+++ src/demo10.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class demo10 {
+    public static void main(String args[]) {
+        //sort list on all letters
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Novosad");
+        list.add("Alexeev");
+        list.add("Potapov");
+        list.add("Navosad");
+        list.add("Alaxaev");
+        list.add("Patapov");
+        Collections.sort(list);
+        String letter = list.get(0).substring(0, 1);
+        boolean redline = true;
+        for (String word : list) {
+            if (!word.substring(0, 1).equals(letter)) {
+                redline = true;
+                letter = word.substring(0, 1);
+            }
+            if (redline) {
+                System.out.println(word);
+            } else {
+                System.out.println(word);
+            }
+            redline = false;
+        }
+    }
+}
---
+++ src/demo9.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class demo9 {
+    public static void main(String args []){
+        int w=0;
+        Scanner in = new Scanner(System.in);
+
+        //check for the input of integers
+       do {
+           try {
+                System.out.print("Enter size of array: ");
+                in = new Scanner(System.in);
+                w = in.nextInt();
+                if (w < 1) {
+                System.out.println("you enter incorrect size, try again");}
+            }catch(Exception e){ System.out.println("you enter incorrect size, try again");}
+        }while(w<1);
+
+       //insertion strings in array
+        if(w==1){System.out.println("Enter " + w + " element of array: ");}
+        if(w>1) {System.out.println("Enter " + w + " elements of array: ");}
+        String [] s = new String[w];
+        for(int i=0; i<s.length; i++){
+        s [i] = in.next();}
+
+        //deleting [] before output array
+        String string = Arrays.toString(s)
+        .replaceFirst("]", "")
+        .replaceFirst("\\[", "");
+        System.out.println(string+"\n");
+
+        //deleting [] before output sort array
+        Set<String> set = new HashSet<>(Arrays.asList(s));
+        String[] result = set.toArray(new String[set.size()]);
+        String string2 = Arrays.toString(result)
+                .replaceFirst("]", "")
+                .replaceFirst("\\[", "");
+        System.out.println("Sort list: "+ string2);
+    }
+}
