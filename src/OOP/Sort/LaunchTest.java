f9e604a39dfc0bbb16ca586749ff5a4942b555c1
Alexandr Alexeev <fart82@ukr.net>
2018-06-13 02:02:49 +0300

    commit from IJ

---
+++ src/OOP/Sort/LaunchTest.java
@@ -0,0 +1,37 @@
+package OOP.Sort;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Arrays;
+
+class Tests {
+
+    private Main main = new Main();
+    private Object [] string1 = {"B", "C", "A"};
+    private Object [] string2 = {"A", "B", "C"};
+    private Object [] string3 = main.test(string1);
+
+    public void testLaunch() {
+        setUp();
+        main();
+        tearDown();
+    }
+
+    @Before
+    public void setUp(){ System.out.println("Tests start");}
+
+    @After
+    public void tearDown(){System.out.println("Tests finish");}
+
+    @Test
+    public void main() {
+        System.out.println((Arrays.equals(string2, string3)) ? "Test1 OK" : "Test1 failed");
+    }
+}
+public class LaunchTest{
+    public static void main(String[] args) {
+        Tests tests = new Tests();
+        tests.testLaunch();
+    }
+}
--- src/OOP/Sort/Main.java
+++ src/OOP/Sort/Main.java
@@ -6,6 +6,11 @@ public class Main {
     private static Object toString(Object[] object){
         return Arrays.toString(object);
     }
+    public Object [] test(Object[] strings){
+        Sort sort = new Sort();
+        sort.sort(strings);
+        return strings;
+    }
 
     public static void main(String[] args) {
         Sort sort = new Sort();
@@ -13,6 +18,6 @@ public class Main {
         System.out.println(toString(sort.sort(new String [] {"B", "C", "A"})));
         System.out.println(toString(sort.sort(new String [] {"ACA", "ABA", "ABC"})));
         System.out.println(toString(sort.sort(new Integer [] {3, 2 , 1})));
-        System.out.println(toString(sort.sort(new Integer [] {33, 32 , 31})));
+        System.out.println(toString(sort.sort(new Object[] {33, 32 , 31})));
     }
 }
