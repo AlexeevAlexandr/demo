f2a91c4f0a4d9d4ba1097cbf3584ed1ced08216d
Alexandr Alexeev <fart82@ukr.net>
2018-06-13 19:19:30 +0300

    commit from IJ

--- src/OOP/Sort/Main.java
+++ src/OOP/Sort/Main.java
@@ -6,10 +6,10 @@ public class Main {
     private static Object toString(Object[] object){
         return Arrays.toString(object);
     }
-    public Object [] test(Object[] strings){
+    public Object [] test(Object[] object){
         Sort sort = new Sort();
-        sort.sort(strings);
-        return strings;
+        sort.sort(object);
+        return object;
     }
 
     public static void main(String[] args) {
@@ -18,6 +18,8 @@ public class Main {
         System.out.println(toString(sort.sort(new String [] {"B", "C", "A"})));
         System.out.println(toString(sort.sort(new String [] {"ACA", "ABA", "ABC"})));
         System.out.println(toString(sort.sort(new Integer [] {3, 2 , 1})));
-        System.out.println(toString(sort.sort(new Object[] {33, 32 , 31})));
+        System.out.println(toString(sort.sort(new Object [] {33, 32 , 31})));
+        System.out.println(toString(sort.sort(new Double [] {3.3, 3.2 , 3.1})));
+        System.out.println(toString(sort.sort(new Object [] {true, true , false})));
     }
 }
